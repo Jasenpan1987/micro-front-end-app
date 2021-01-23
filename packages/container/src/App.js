8fd843c11fe7853060453dcc34a38803ac687d3b
Jasen Pan <jasenpan@gmail.com>
2021-01-23 22:54:24 +1100

    swap

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -10,14 +10,14 @@ const generateClassName = createGenerateClassName({
 
 const App = () => {
   return (
-    <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
           <MarketingApp />
         </div>
-      </BrowserRouter>
-    </StylesProvider>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
 

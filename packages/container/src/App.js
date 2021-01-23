00c8769646fb7b4d89b50bfc10d21d1915f8b6f2
Jasen Pan <jasenpan@gmail.com>
2021-01-23 22:49:51 +1100

    fix class name collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./Components/MarketingApp";
 import Header from "./Components/Header";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "containerApp",
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,18 @@
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Landing from "./Landing";
 import Pricing from "./Pricing";
 
+const generatedClassName = createGenerateClassName({
+  productionPrefix: "marketingApp",
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generatedClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Landing} />
